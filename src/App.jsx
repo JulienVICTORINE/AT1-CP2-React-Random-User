75d585d81dc0605a837206ef26d80adf107aa80e
julien <julien.victorine440@gmail.com>
2025-04-22 16:32:45 +0400

    afficher plusieurs utilisateurs

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState, useEffect } from 'react';
 import Card from './Card';
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
+  const [users, setUsers] = useState([]);
 
   const fetchUserData = async () => {
     try {
-      const request = await fetch("https://randomuser.me/api/");
+      const request = await fetch("https://randomuser.me/api/?results=10");
       const response = await request.json();
-      const userData = response.results[0]; // j'affiche un utilisateur dans un premier temps
-      setUser(userData);
+      const userData = response.results;
+      setUsers(userData);
     } catch (error) {
       console.log(error);
     }
@@ -20,20 +21,25 @@ function App() {
     fetchUserData();
   }, []);
 
-  if (!user) {
+  if (users.length == 0) {
     return <p style={{ color: "white" }}>Chargement...</p>;
   }
 
   return (
     <>
       <h1>RandomUser</h1>
-      <Card
-        img={user.picture.large}
-        name={`${user.name.first} ${user.name.last}`}
-        age={user.dob.age}
-        location={`${user.location.city}, ${user.location.country}`}
-        email={user.email}
-      />
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "1rem", justifyContent: "center" }}>
+        {users.map((user, index) => (
+          <Card
+            key={index}
+            img={user.picture.large}
+            name={`${user.name.first} ${user.name.last}`}
+            age={user.dob.age}
+            location={`${user.location.city}, ${user.location.country}`}
+            email={user.email}
+          />
+        ))}
+      </div>    
     </>
   );
 }
